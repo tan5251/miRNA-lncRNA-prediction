c187b67e6dce647a109b79a538792af9afb45818
tan5251 <74244525+tan5251@users.noreply.github.com>
2021-04-23 15:21:06 +0800

    Create code.sh

---
+++ code.sh
@@ -0,0 +1,8 @@
+#miRanda
+tar xvzf miRanda-aug2010.tar.gz
+cd ./path/to/miRanda
+./configure --prefix=/public/home/tanbh/ChIP-seq/miRNA/miRanda/
+cd /public/home/tanbh/ChIP-seq/miRNA/miRanda/
+./bin/miRanda -help
+
+##
